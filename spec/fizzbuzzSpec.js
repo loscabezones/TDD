24878613db7be3a79e2fd7e1273d7bbeb30b79d0
USERSAD\loscabezones <david_gaia2@hotmail.com>
2018-11-09 13:53:33 +0100

    reacer ejercicio de jasmin pero isntalado con node

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,31 @@
+describe("FizzBuzz", function () {
+
+    var FizzBuzz = require('../ejercicios/fizzbuzz');
+
+    it('should return Fizz for 3', function () {
+        var result = FizzBuzz.check(3);
+        expect(result).toEqual('Fizz');
+    });
+
+    it('should return Fizz for 9', function () {
+        var result = FizzBuzz.check(9);
+        expect(result).toEqual('Fizz');
+    });
+
+    it('should return Fizz for 5', function () {
+        var result = FizzBuzz.check(5);
+        expect(result).toEqual('Buzz');
+    });
+
+    it('should return FizzBuzz for 15', function () {
+        var result = FizzBuzz.check(15);
+        expect(result).toEqual('FizzBuzz');
+    });
+
+    it( 'should return Zero for 0', function() {
+        var result = FizzBuzz.check( 0 );
+        expect( result ).toEqual( 'Zero' );
+      });
+
+
+});
